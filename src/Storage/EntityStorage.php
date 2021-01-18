33656e4ca887532e3ce6c2a0e0bdb705db8740fa
M. Vondano <m-vo@users.noreply.github.com>
2021-01-18 12:49:28 +0100

    refactor entity storage

--- src/Storage/EntityStorage.php
+++ src/Storage/EntityStorage.php
@@ -10,59 +10,31 @@ declare(strict_types=1);
 namespace Mvo\ContaoGroupWidget\Storage;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\Mapping\ClassMetadata;
 use Mvo\ContaoGroupWidget\Entity\GroupElementEntityInterface;
-use Mvo\ContaoGroupWidget\Entity\GroupEntityInterface;
-use Mvo\ContaoGroupWidget\Group\Group;
-use Psr\Container\ContainerInterface;
-use Symfony\Component\PropertyAccess\PropertyAccess;
-use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Mvo\ContaoGroupWidget\Entity\GroupEntityProxy;
 
 /**
  * Storage adapter to store group/element data using entity classes.
  */
 final class EntityStorage implements StorageInterface
 {
-    private ContainerInterface $locator;
+    private EntityManagerInterface $entityManager;
 
-    private PropertyAccessor $propertyAccessor;
+    private GroupEntityProxy $groupEntityProxy;
+    private string $elementEntity;
 
-    private Group $group;
-
-    private ClassMetadata $groupMetadata;
-    private ClassMetadata $elementMetadata;
-    private ?GroupEntityInterface $groupEntity = null;
-
-    public function __construct(ContainerInterface $locator, string $entity, Group $group)
+    public function __construct(EntityManagerInterface $entityManager, GroupEntityProxy $groupEntityProxy, string $elementEntity)
     {
-        $this->locator = $locator;
-        $this->propertyAccessor = PropertyAccess::createPropertyAccessor();
+        $this->entityManager = $entityManager;
 
-        $metadataFactory = $this->entityManager()->getMetadataFactory();
-
-        // Retrieve group entity metadata
-        $this->groupMetadata = $metadataFactory->getMetadataFor($entity);
-
-        if (!$this->groupMetadata->getReflectionClass()->implementsInterface(GroupEntityInterface::class)) {
-            throw new \RuntimeException(sprintf("Group entity class for group '%s' must implement '%s'.", $group->getName(), GroupEntityInterface::class));
-        }
-
-        // Retrieve element entity metadata
-        $this->elementMetadata = $metadataFactory->getMetadataFor(
-            $this->groupMetadata->getAssociationTargetClass('elements')
-        );
-
-        if (!$this->elementMetadata->getReflectionClass()->implementsInterface(GroupElementEntityInterface::class)) {
-            throw new \RuntimeException(sprintf("Element entity class for group '%s' must implement '%s'.", $group->getName(), GroupElementEntityInterface::class));
-        }
-
-        $this->group = $group;
+        $this->groupEntityProxy = $groupEntityProxy;
+        $this->elementEntity = $elementEntity;
     }
 
     public function getElements(): array
     {
         // Sort elements by position field
-        $elements = $this->getGroupEntity()->getElements()->toArray();
+        $elements = $this->groupEntityProxy->getElements()->toArray();
 
         usort(
             $elements,
@@ -87,17 +59,21 @@ final class EntityStorage implements StorageInterface
     public function createElement(): int
     {
         /** @var GroupElementEntityInterface $element */
-        $element = $this->elementMetadata->getReflectionClass()->newInstance();
+        $element = $this->entityManager
+            ->getMetadataFactory()
+            ->getMetadataFor($this->elementEntity)
+            ->getReflectionClass()
+            ->newInstance()
+        ;
 
+        // Position at the end
         $element->setPosition(0);
 
-        $groupEntity = $this->getGroupEntity();
-        $groupEntity->addElement($element);
+        $this->groupEntityProxy->addElement($element);
 
         // Persist and flush to make ID available
-        $manager = $this->entityManager();
-        $manager->persist($element);
-        $manager->flush();
+        $this->entityManager->persist($element);
+        $this->entityManager->flush();
 
         $id = $element->getId();
 
@@ -110,12 +86,10 @@ final class EntityStorage implements StorageInterface
 
     public function removeElement(int $elementId): void
     {
-        $groupEntity = $this->getGroupEntity();
-
         /** @var GroupElementEntityInterface $element */
-        foreach ($groupEntity->getElements() as $element) {
+        foreach ($this->groupEntityProxy->getElements() as $element) {
             if ($elementId === $element->getId()) {
-                $groupEntity->removeElement($element);
+                $this->groupEntityProxy->removeElement($element);
 
                 return;
             }
@@ -129,7 +103,7 @@ final class EntityStorage implements StorageInterface
         $elementsById = [];
 
         /** @var GroupElementEntityInterface $element */
-        foreach ($this->getGroupEntity()->getElements() as $element) {
+        foreach ($this->groupEntityProxy->getElements() as $element) {
             $id = $element->getId();
 
             if (null !== $id) {
@@ -149,9 +123,12 @@ final class EntityStorage implements StorageInterface
     public function getField(int $elementId, string $field)
     {
         /** @var GroupElementEntityInterface $element */
-        foreach ($this->getGroupEntity()->getElements() as $element) {
+        foreach ($this->groupEntityProxy->getElements() as $element) {
             if ($element->getId() === $elementId) {
-                return $this->propertyAccessor->getValue($element, $field);
+                $property = new \ReflectionProperty($element, $field);
+                $property->setAccessible(true);
+
+                return $property->getValue($element);
             }
         }
 
@@ -161,9 +138,12 @@ final class EntityStorage implements StorageInterface
     public function setField(int $elementId, string $field, $value): void
     {
         /** @var GroupElementEntityInterface $element */
-        foreach ($this->getGroupEntity()->getElements() as $element) {
+        foreach ($this->groupEntityProxy->getElements() as $element) {
             if ($element->getId() === $elementId) {
-                $this->propertyAccessor->setValue($element, $field, $value);
+                $property = new \ReflectionProperty($element, $field);
+                $property->setAccessible(true);
+
+                $property->setValue($element, $value);
 
                 return;
             }
@@ -174,55 +154,13 @@ final class EntityStorage implements StorageInterface
 
     public function persist(): void
     {
-        $this->entityManager()->flush();
+        $this->entityManager->flush();
     }
 
     public function remove(): void
     {
-        $manager = $this->entityManager();
+        $this->entityManager->remove($this->groupEntityProxy->getReference());
 
-        $manager->remove($this->getGroupEntity());
-
-        $manager->flush();
-    }
-
-    private function getGroupEntity(): GroupEntityInterface
-    {
-        if (null !== $this->groupEntity) {
-            return $this->groupEntity;
-        }
-
-        $manager = $this->entityManager();
-
-        $query = $manager->createQueryBuilder()
-            ->select('g')
-            ->from($this->groupMetadata->getName(), 'g')
-            ->where('g.sourceTable = :table AND g.sourceId = :id')
-            ->setParameters([
-                'table' => $this->group->getTable(),
-                'id' => $this->group->getRowId(),
-            ])
-            ->setMaxResults(1)
-            ->getQuery()
-        ;
-
-        if (null !== ($result = $query->getResult()[0] ?? null)) {
-            $entity = $result;
-        } else {
-            /** @var GroupEntityInterface $entity */
-            $entity = $this->groupMetadata->getReflectionClass()->newInstance();
-
-            $entity->setSourceTable($this->group->getTable());
-            $entity->setSourceId($this->group->getRowId());
-
-            $manager->persist($entity);
-        }
-
-        return $this->groupEntity = $entity;
-    }
-
-    private function entityManager(): EntityManagerInterface
-    {
-        return $this->locator->get('doctrine.orm.entity_manager');
+        $this->entityManager->flush();
     }
 }
---
+++ src/Storage/EntityStorageFactory.php
@@ -0,0 +1,152 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @author  Moritz Vondano
+ * @license MIT
+ */
+
+namespace Mvo\ContaoGroupWidget\Storage;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Mvo\ContaoGroupWidget\Entity\GroupEntityProxy;
+use Mvo\ContaoGroupWidget\Group\Group;
+
+class EntityStorageFactory implements StorageFactoryInterface
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public static function getName(): string
+    {
+        return 'entity';
+    }
+
+    public function create(Group $group): EntityStorage
+    {
+        $entityClass = $group->getDefinition('entity');
+
+        if (null === $entityClass) {
+            // If no entity reference is defined, assume that there is an
+            // entity backing the current DCA with a field for the group.
+            [$entity, $targetMapping] = $this->getLocalEntity($group);
+        } else {
+            // Otherwise try to locate a referenced DCA via the 'entity' key.
+            [$entity, $targetMapping] = $this->getReferencedEntity($entityClass, $group);
+        }
+
+        // Wrap entity into a proxy that handles accessing and manipulating the
+        // element association. The entity must have methods that follow a
+        // contract (get<Elements>/add<Element>/remove<Element>).
+        $groupEntityProxy = new GroupEntityProxy($entity, $targetMapping['fieldName']);
+
+        // Get the class of the element entity (the target in the association).
+        $elementEntity = $targetMapping['targetEntity'];
+
+        return new EntityStorage($this->entityManager, $groupEntityProxy, $elementEntity);
+    }
+
+    private function getLocalEntity(Group $group)
+    {
+        $name = $group->getName();
+        $table = $group->getTable();
+
+        // Find metadata for the entity that matches the group's table
+        $classMetadata = (
+        function () use ($table, $name): ClassMetadata {
+            /** @var ClassMetadata $metadata */
+            foreach ($this->entityManager->getMetadataFactory()->getAllMetadata() as $metadata) {
+                if ($table === $metadata->getTableName()) {
+                    return $metadata;
+                }
+            }
+
+            throw new \InvalidArgumentException("There is no entity for table '$table'. Did you forget an 'entity' definition for group '$name'?");
+        }
+        )();
+
+        if (!$classMetadata->hasAssociation($name)) {
+            throw new \InvalidArgumentException("Entity '{$classMetadata->getName()}' does not contain a field for group '$name'.");
+        }
+
+        $targetMapping = $classMetadata->getAssociationMapping($name);
+
+        // Get current instance
+        $query = $this->entityManager->createQueryBuilder()
+            ->select('g')
+            ->from($classMetadata->getName(), 'g')
+            ->where('g.id = :id')
+            ->setParameters(['id' => $group->getRowId()])
+            ->setMaxResults(1)
+            ->getQuery()
+        ;
+
+        if (null !== ($result = $query->getResult()[0] ?? null)) {
+            return [$result, $targetMapping];
+        }
+
+        throw new \RuntimeException("No group record was found for table '{$group->getTable()}' ID {$group->getRowId()}.");
+    }
+
+    private function getReferencedEntity(string $referencedEntity, Group $group)
+    {
+        // Find metadata for the referenced entity
+        $classMetadata = $this->entityManager
+            ->getMetadataFactory()
+            ->getMetadataFor($referencedEntity)
+        ;
+
+        if (!$classMetadata->hasAssociation('elements')) {
+            throw new \InvalidArgumentException("Entity '{$classMetadata->getName()}' does not contain a field 'elements' referencing group elements of group '{$group->getName()}'.");
+        }
+
+        $targetMapping = $classMetadata->getAssociationMapping('elements');
+
+        if (!$classMetadata->hasField('sourceTable') || !$classMetadata->hasField('sourceId')) {
+            throw new \InvalidArgumentException("Entity '{$classMetadata->getName()}' needs to contain fields 'sourceTable' and 'sourceId' in order to associate field group '{$group->getName()}'.");
+        }
+
+        // Get current instance
+        $query = $this->entityManager->createQueryBuilder()
+            ->select('g')
+            ->from($classMetadata->getName(), 'g')
+            ->where('g.sourceTable = :table AND g.sourceId = :id')
+            ->setParameters([
+                'table' => $group->getTable(),
+                'id' => $group->getRowId(),
+            ])
+            ->setMaxResults(1)
+            ->getQuery()
+        ;
+
+        if (null !== ($result = $query->getResult()[0] ?? null)) {
+            return [$result, $targetMapping];
+        }
+
+        // Create a new instance
+        $properties = [
+            'sourceTable' => $group->getTable(),
+            'sourceId' => $group->getRowId(),
+        ];
+
+        $reflectionClass = $classMetadata->getReflectionClass();
+        $entity = $reflectionClass->newInstance();
+
+        foreach ($properties as $name => $value) {
+            $property = $reflectionClass->getProperty($name);
+            $property->setAccessible(true);
+
+            $property->setValue($entity, $value);
+        }
+
+        $this->entityManager->persist($entity);
+
+        return [$entity, $targetMapping];
+    }
+}
